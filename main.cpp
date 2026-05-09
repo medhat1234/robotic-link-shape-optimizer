71f533e4710eb2d636ef3e211a9eabaeaea29e41
medhat <medhatetman6@gmail.com>
2026-05-09 22:49:50 +0300

    angular velocity added by hassouna

--- main.cpp
+++ main.cpp
@@ -30,8 +30,8 @@ struct Motor {
 
 struct Gearbox {
     string name;
-    double ratio;
     double efficiency;
+    double ratio;
     double mass;     // kg
     double diameter; // mm
     double width;    // mm
@@ -39,7 +39,7 @@ struct Gearbox {
 
 vector<Motor> motors = {
       // {"name",torque in N m, speedRpm, mass_kg, diameter mm , width mm}
-    {"IDX 70 S, brushless, 600 W",  1.400, 5000,  1.595,  70, 125.5},
+    {"IDX 70 S, brushless, 600 W",  1.400, 5000,    1.595,  70, 125.5},
     {"IDX 70 M, brushless, 800 W",  2.480, 3670,    2.295,  70, 160.5},
     {"IDX 70 L, brushless, 900 W",  3.710, 2430,    2.995,  70, 195.5},
     {"IDX 56 L, brushless, 400 W",  1.040, 3430,    1.196,  56, 160},
@@ -50,12 +50,12 @@ vector<Motor> motors = {
 };
 
 vector<Gearbox> gearboxes = {
-       // {"name", efficiency, gearRatio, mass_kg, diameter mm , width mm}
-    {"Planetary Gearhead GP 16 A, Metal Version, Sleeve Bearing", 0.85, 4.4, 0.020, 16, 18.1},
-    {"Planetary Gearhead GP 22 A, Metal Version", 0.59, 84, 0.068, 22, 40},
-    {"Planetary Gearhead GP 22 C", 0.70, 14, 0.055, 22, 35.35},
-    {"Planetary Gearhead GP 32 A, Metal Version", 0.60, 246, 0.226, 32, 54.85},
-    {"Planetary Gearhead GP 42 C, 3 - 15 Nm, Ceramic Version", 0.72, 43, 0.460, 42, 73}
+       // {"name",                                                  efficiency, gearRatio,  mass_kg, diameter mm , width mm}
+    {"Planetary Gearhead GP 16 A, Metal Version, Sleeve Bearing",   0.85,       4.4,        0.020,   16,           18.1},
+    {"Planetary Gearhead GP 22 A, Metal Version",                   0.59,       84,         0.068,   22,           40},
+    {"Planetary Gearhead GP 22 C",                                  0.70,       14,         0.055,   22,           35.35},
+    {"Planetary Gearhead GP 32 A, Metal Version",                   0.60,       246,        0.226,   32,           54.85},
+    {"Planetary Gearhead GP 42 C, 3 - 15 Nm, Ceramic Version",      0.72,       43,         0.460,   42,           73}
 
 
 };
@@ -127,40 +127,43 @@ void optimizeLink(Material mat, int type, double& dim1_mm, double& dim2_mm, doub
     cout << "Final Stress: " << sigma_pa / 1e6 << " MPa" << endl;
 }
 
-void selectActuation(double ml_kg, double L_mm, double mp_kg, double alpha, const vector<Motor>& motors, const vector<Gearbox>& gearboxes) {
-    double L_m = L_mm / 1000.0;
-    double T_req = (ml_kg * G * (L_m / 2.0)) + (mp_kg * G * L_m) + (ml_kg * pow(L_m / 2.0, 2) * alpha) + (mp_kg * pow(L_m, 2) * alpha);
-
-    double bestCost = 1e18;
-    string bestCombo = "None";
-
-    for (const auto& m : motors) {
-        for (const auto& g : gearboxes) {
-            double T_out = m.torque * g.ratio * g.efficiency;
-
-            if (T_out >= T_req) {
-                double totalMass = m.mass + g.mass;
-                double maxDiam = max(m.diameter, g.diameter);
-                double totalWidth = m.width + g.width;
+void selectActuation(double Arm_mass_kg, double Link_length_mm, double Paylaod_mass_kg, double Angular_acceleration, double Angular_velocity_req ,const vector<Motor>& motors, const vector<Gearbox>& gearboxes) {
+    // convert everything to SI units for torque calculation.
+    double Link_length_m = Link_length_mm / 1000.0;
+    double Torque_req = (Arm_mass_kg * G * (Link_length_m / 2.0)) + (Paylaod_mass_kg * G * Link_length_m) + (Arm_mass_kg * pow(Link_length_m / 2.0, 2) * Angular_acceleration)
+                        + (Paylaod_mass_kg * pow(Link_length_m, 2) * Angular_acceleration);
+    // compareing between motors and gearboxes.
+    double bestCost = 1e18; // limit of the cost .
+    string bestCombo ;
+    // by using range based for loops.
+    for (const auto &motor : motors) {
+        for (const auto &gearbox : gearboxes) {
+            double Torque_out = motor.torque * gearbox.ratio * gearbox.efficiency;
+            //the condition checks if the motor-gearbox combo can meet both the torque and speed requirements.
+            if (Torque_out >= Torque_req && (motor.speed / gearbox.ratio) >= ((Angular_velocity_req*60)/(2*PI)) ){
+                double totalMass = motor.mass + gearbox.mass;
+                // We use the larger diameter of the Gearbox or Motor.
+                double maxDiam = max(motor.diameter, gearbox.diameter);
+                double totalWidth = motor.width + gearbox.width;
                 double cost = totalMass + (maxDiam / 100.0) + (totalWidth / 100.0);
 
                 if (cost < bestCost) {
                     bestCost = cost;
-                    bestCombo = m.name + " + " + g.name;
+                    bestCombo = motor.name + " + " + gearbox.name;
                 }
             }
         }
     }
 
     cout << "\n--- Actuation Selection ---" << endl;
-    cout << "Required Torque: " << T_req << " Nm" << endl;
+    cout << "Required Torque: " << Torque_req << " Nm" << endl;
     cout << "Best Combination: " << bestCombo << " (Cost Score: " << bestCost << ")" << endl;
 }
 
 int main() {
 
     int shapeChoice, matChoice;
-    double L, mp, alpha, d1, d2 = 0;
+    double L, mp, alpha, angular_velocity_req, d1, d2 = 0;
     Material selectedMaterial;
     string entry;
 
@@ -188,7 +191,9 @@ int main() {
     // 3. Dimensional Inputs
     cout << "Link Length (mm): "; cin >> L;
     cout << "Payload Mass (kg): "; cin >> mp;
+    cout << "Required Angular Velocity (rad/s): "; cin >> angular_velocity_req;
     cout << "Max Acceleration (rad/s^2): "; cin >> alpha;
+
 
     if (shapeChoice) {
         cout << "Initial Width (mm): "; cin >> d1;
@@ -207,7 +212,7 @@ int main() {
     double d2_m = d2 / 1000.0;
     double ml = (shapeChoice == 1) ? rho*(d1_m*d2_m*L_m) : rho*(PI*pow(d1_m,2)*L_m);
 
-    selectActuation(ml, L, mp, alpha, motors, gearboxes);
+    selectActuation(ml, L, mp, alpha, angular_velocity_req, motors, gearboxes);
 
     cout<<"enter anything to close"<<endl;
     cin>>entry; //to avoid auto close
